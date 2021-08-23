fbefc35fe3c12bf21b516b35be00fb8ba0b5519d
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-08-23 12:21:19 -0300

    FORMAS DE REALIZAR DECLARAÇÕES DE FUNÇÕES

--- aula40/index.js
+++ aula40/index.js
@@ -1,1 +1,31 @@
 //Seção 4: JavaScript Funções (Avançado)
+
+// DECLARAÇÃO DE FUNÇÃO HOISTING
+falaOi();
+function falaOi(){
+    console.log('Oie');
+}
+
+//FRIST-CLASS OBJECTS (Objeto primeira classe)
+// Function expression
+
+const souUmDado = function(){
+    console.log('Sou dado .')
+}
+souUmDado();
+
+//Arraw funcão (Function hoisting)
+
+const funcaoArrow= ()=> {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const obj = {
+    falar: function(){
+        console.log('Estou falando ...')
+    }
+    
+};
+obj.falar();
