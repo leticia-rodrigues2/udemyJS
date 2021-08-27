f337fb6421a3da158e794db2ca8b9152fac3ac29
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-08-27 19:07:48 -0300

    Funções construtoras (constructor functions)

---
+++ aula47/index.js
@@ -0,0 +1,25 @@
+// função construtora -> objetos
+// Função fabrica -> objetos
+//Construtora -> Pessoa (new)
+
+function Pessoa(nome , sobrenome){
+    
+    // Atributos ou métodos Privadas
+    const ID = 123456;
+    const metodoInterno = function (){
+
+    };
+
+    // Atributos ou métodos públicos
+    this.nome = nome ;
+    this.sobrenome = sobrenome ;
+
+    this.metodo = function(){
+        console.log(this.nome + ': sou um método');
+
+    };
+}
+const p1 = new Pessoa ('Leticia', 'Rodrigues');
+const p2 = new Pessoa ('Maria', 'Oliveira');
+p1.metodo();
+p2.metodo();
