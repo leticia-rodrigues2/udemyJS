d9b6a41ec773e9531245ebfd0cefeee5376547ff
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-09-03 19:08:09 -0300

    Prototypes

---
+++ aula61/index.js
@@ -0,0 +1,18 @@
+function Pessoa (nome,sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome ;
+    this.nomeCompleto = () => this.nome + 'ORIGINAL' + this.sobrenome;
+
+}
+
+//Pessoa.prototype.estouAqui = 'HAHAHAHAHA';
+Pessoa.prototype.nomeCompleto = function () {
+    return this.nome + ' ' + this.nomeCompleto;
+}
+const pessoa1 = new Pessoa ('Luiz' ,'0.'); // <-- Pessoa = Função construtora
+const pessoa2 = new Pessoa ('Maria' ,'0.'); // <-- Pessoa = Função construtora
+
+const data = new Date(); // <-- Date = Função construtora
+
+console.dir(pessoa1);
+console.dir(data);
