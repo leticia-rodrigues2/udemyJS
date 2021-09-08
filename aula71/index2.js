f7594965f3c4bbc00d0402ee8c4573b2aca179e9
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-09-08 14:43:34 -0300

    Getters e Setters

---
+++ aula71/index2.js
@@ -0,0 +1,19 @@
+class Pessoa {
+    constructor(nome,sobrenome){
+        this.nome = nome ;
+        this.sobrenome = sobrenome;
+    }
+
+    get nomeCompleto(){
+        return this.nome + ' ' + this.sobrenome;
+    }
+    set nomeCompleto (valor){
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome = valor.join(' ');
+    }
+}
+
+const p1 = new Pessoa ('Leticia' , 'Rodrigue');
+p1.nomeCompleto =` Leticia Rodrigues Bento`
+console.log(p1.nomeCompleto);
