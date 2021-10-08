49d5e997b4f90c97af64585ba983b622f066a596
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-10-08 14:49:19 -0300

    CRIANDO ARVIVO CSV E TXT

---
+++ criar txt-csv/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let texto = 'euuuuuuuuuuu';
+let titulo ='estooooouuuu';
+function salvar() {
+
+    fs.writeFileSync("./criar txt/salvando2.txt", texto +" "+  titulo ,"utf-8" );
+    fs.writeFileSync("./criar txt/salvandoCsv.csv", texto +" "+  titulo ,"utf-8" );
+   
+}
+salvar()
