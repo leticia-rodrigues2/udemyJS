82086765595e5f7ebb9bf4f82b51c0803247862b
Leticia Rodrigues <leticia.bento@fortbrasil.com.br>
2021-12-17 15:27:29 -0300

    Babel  aula 115

---
+++ aula83/main.js
@@ -0,0 +1,11 @@
+const nome ='Leticia' ;
+const obj= {nome};
+const novoObj ={...obj};
+console.log(novoObj);
+
+class Pessoa {
+  constructor(nome,sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+}
